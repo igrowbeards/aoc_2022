271f8a1c5ee2acccd7fec2353e6f23555d82fc1f
Justin Edwards <justin@igrowbeards.com>
2022-12-02 05:45:54 -0600

    day one solved

---
+++ lib/day_001.ex
@@ -0,0 +1,41 @@
+defmodule Day001 do
+  def read_input() do
+    File.read!("lib/day_001_input.txt")
+  end
+
+  def run_part_1() do
+    read_input()
+    |> parse_input()
+    |> highest_calories()
+  end
+  
+  def run_part_2() do
+    read_input()
+    |> parse_input()
+    |> highest_calories(3)
+  end
+
+  def parse_input(input) do
+    input
+    |> split_elves_list()
+    |> parse_elves_calories()
+  end
+
+  def highest_calories(elf_calories, num_elves \\ 1) do
+    elf_calories
+    |> Enum.map(&Enum.sum/1)
+    |> Enum.sort(:desc)
+    |> Enum.take(num_elves)
+    |> Enum.sum()
+  end
+
+  defp split_elves_list(list), do: String.split(list, "\n\n")
+
+  defp parse_elves_calories(elves_list), do: Enum.map(elves_list, &parse_elf_calories/1)
+
+  defp parse_elf_calories(elf_cal_list) do
+    elf_cal_list
+    |> String.split()
+    |> Enum.map(&String.to_integer/1)
+  end
+end
---
+++ test/day_001_test.exs
@@ -0,0 +1,42 @@
+defmodule Day001Test do
+  use ExUnit.Case
+
+  @raw  """
+  1000
+  2000
+  3000
+
+  4000
+
+  5000
+  6000
+
+  7000
+  8000
+  9000
+
+  10000
+  """
+
+  test "parse input" do
+    expected = [
+      [1000, 2000, 3000],
+      [4000],
+      [5000, 6000],
+      [7000, 8000, 9000],
+      [10000]
+    ]
+
+    assert Day001.parse_input(@raw) == expected
+  end
+
+  test "highest elf calories" do
+    input = Day001.parse_input(@raw)
+    assert Day001.highest_calories(input) == 24_000
+  end
+
+  test "highest n elf calories" do
+    input = Day001.parse_input(@raw)
+    assert Day001.highest_calories(input, 3) == 24_000 + 11_000 + 10_000
+  end
+end
