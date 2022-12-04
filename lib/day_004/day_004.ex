7ba39ad099cb1c0233b16b9bcfcbbc6ac7740594
Justin Edwards <justin@igrowbeards.com>
2022-12-04 07:53:25 -0600

    f

--- lib/day_004/day_004.ex
+++ lib/day_004/day_004.ex
@@ -12,7 +12,6 @@ defmodule Day004 do
   def parse_and_count(str, count_fun) do
     str
     |> String.split()
-    |> Enum.map(&parse_line/1)
     |> Enum.count(count_fun)
   end
 
@@ -23,23 +22,32 @@ defmodule Day004 do
     |> List.to_tuple()
   end
 
+  # parse and check
+  def assignments_overlap_totally?(input) when is_binary(input) do
+    input
+    |> parse_line
+    |> assignments_overlap_totally?()
+  end
+
   def assignments_overlap_totally?({s1..e1, s2..e2}) do
     (s1 <= s2 && e1 >= e2) || (s2 <= s1 && e2 >= e1)
   end
 
-  def assignments_overlap?({a, b}), do: !Range.disjoint?(a, b)
-
-  defp input() do
-    "lib/day_004/day_004_input.txt"
-    |> File.read!()
+  # parse and check
+  def assignments_overlap?(input) when is_binary(input) do
+    input
+    |> parse_line()
+    |> assignments_overlap?()
   end
 
-  defp to_range(input) do
-    [start, finish] =
-      input
-      |> String.split("-")
-      |> Enum.map(&String.to_integer/1)
+  def assignments_overlap?({a, b}), do: !Range.disjoint?(a, b)
 
-    Range.new(start, finish)
+  defp input(), do: File.read!("lib/day_004/day_004_input.txt")
+
+  defp to_range(input) do
+    input
+    |> String.split("-")
+    |> Enum.map(&String.to_integer/1)
+    |> (fn [a, b] ->  Range.new(a, b) end).()
   end
 end
